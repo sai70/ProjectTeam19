278edf1fa763613d39ecf7ba3960ad4b34618c03
unknown <Sagar>
2015-09-24 17:12:14 -0700

    initial commit

---
+++ app/src/main/java/com/example/sagar/projectteam19/BuyerLoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.sagar.projectteam19;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sagar on 9/24/2015.
+ */
+public class BuyerLoginActivity extends AppCompatActivity {
+
+}
